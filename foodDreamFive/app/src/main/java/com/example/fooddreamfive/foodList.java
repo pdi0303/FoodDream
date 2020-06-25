f0d199eda3a43192b044979569d51c102f460bc5
pdi0303 <pdi0303@hansung.ac.kr>
2020-06-25 10:13:53 +0900

    푸드 드림

---
+++ foodDreamFive/app/src/main/java/com/example/fooddreamfive/foodList.java
@@ -0,0 +1,29 @@
+package com.example.fooddreamfive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class foodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.foodlist);
+
+        ImageButton macaronImage = findViewById(R.id.macaronImage);
+        macaronImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        foodInfo.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+    }
+}
